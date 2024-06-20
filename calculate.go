e26a4066d69bd5ed53a4595248b48713a57c9085
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-06-19 21:45:26 -0700

    reorganized project

---
+++ calculate.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// Calculates points based on total price
+func pointsFromTotal(receipt Receipt) (int, error) {
+	score := 0
+
+	// Convert total to float
+	total, err := strconv.ParseFloat(receipt.Total, 64)
+
+	if err != nil {
+		return 0, fmt.Errorf("error converting total to float: %v", err)
+	}
+
+	// Adds 50 points if total is round dollar amount
+	if int(total*100)%100 == 0 {
+		score += 50
+	}
+
+	// Add 25 points if total is multiple of 0.25
+	if int(total*100)%25 == 0 {
+		score += 25
+	}
+
+	return score, nil
+}
+
+// Calculates points from length of description
+func pointsFromDescription(receipt Receipt) (int, error) {
+	score := 0
+
+	// Add price * 0.2 if trimmed length of item description % 3 == 0
+	for _, item := range receipt.Items {
+		trimmed := strings.TrimSpace(item.ShortDescription)
+		if len(trimmed)%3 == 0 {
+			price, err := strconv.ParseFloat(item.Price, 64)
+
+			if err != nil {
+				return 0, fmt.Errorf("error converting price to float: %v", err)
+			}
+
+			score += int(math.Ceil(price * 0.2))
+		}
+	}
+
+	return score, nil
+}
+
+// Calculates points based on if day is odd
+func pointsFromDay(receipt Receipt) (int, error) {
+	score := 0
+
+	// Parse date string for day
+	dayString := strings.Split(receipt.PurchaseDate, "-")[2]
+	day, err := strconv.ParseInt(dayString, 10, 8)
+
+	if err != nil {
+		return 0, fmt.Errorf("unable to convert day to int: %v", err)
+	}
+
+	// Add 6 points if day in purchase date is odd
+	if day%2 == 1 {
+		score += 6
+	}
+
+	return score, nil
+}
+
+// Calculates the score associated with a given receipt
+func calculateScore(receipt Receipt) (int, error) {
+	score := 0
+
+	// Adds 1 point for each alphanumeric characters of retailer
+	for _, c := range receipt.Retailer {
+		if unicode.IsLetter(c) || unicode.IsDigit(c) {
+			score += 1
+		}
+	}
+
+	// Add points from total
+	points, _ := pointsFromTotal(receipt)
+	score += points
+
+	// Add 5 points for every two items on receipt
+	score += 5 * (len(receipt.Items) / 2)
+
+	// Add points from description length
+	points, _ = pointsFromDescription(receipt)
+	score += points
+
+	// Add points from day value
+	points, _ = pointsFromDay(receipt)
+	score += points
+
+	// Add 10 points if time of purchase is after 2pm and before 4pm
+	time := strings.Split(receipt.PurchaseTime, ":")
+
+	if time[0] == "14" && time[1] != "00" || time[0] == "15" {
+		score += 10
+	}
+
+	return score, nil
+}
--- main.go
+++ main.go
@@ -4,25 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
 	"os"
-	"strconv"
-	"strings"
-	"unicode"
 )
-
-type Item struct {
-	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"price"`
-}
-
-type Receipt struct {
-	Retailer     string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDate"`
-	PurchaseTime string `json:"purchaseTime"`
-	Items        []Item `json:"items"`
-	Total        string `json:"total"`
-}
 
 func deserialzeJSON(filepath string) (Receipt, error) {
 	var receipt Receipt
@@ -30,138 +13,37 @@ func deserialzeJSON(filepath string) (Receipt, error) {
 	// Open JSON file
 	file, err := os.Open(filepath)
 	if err != nil {
-		return receipt, fmt.Errorf("unable to open file: %v\n", err)
+		return receipt, fmt.Errorf("unable to open file: %v", err)
 	}
 	defer file.Close()
 
 	// Read file content
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		return receipt, fmt.Errorf("unable to open file: %v\n", err)
+		return receipt, fmt.Errorf("unable to open file: %v", err)
 	}
 
 	// Deserialize struct
 	err = json.Unmarshal(bytes, &receipt)
 	if err != nil {
-		return receipt, fmt.Errorf("unable to deserialize JSON: %v\n", err)
+		return receipt, fmt.Errorf("unable to deserialize JSON: %v", err)
 	}
 
 	return receipt, nil
 }
 
-// Calculates points based on total price
-func pointsFromTotal(receipt Receipt) (int, error) {
-	score := 0
-
-	// Convert total to float
-	total, err := strconv.ParseFloat(receipt.Total, 64)
-
-	if err != nil {
-		return 0, fmt.Errorf("Error converting total to float: %v\n", err)
-	}
-
-	// Adds 50 points if total is round dollar amount
-	if int(total*100)%100 == 0 {
-		score += 50
-	}
-
-	// Add 25 points if total is multiple of 0.25
-	if int(total*100)%25 == 0 {
-		score += 25
-	}
-
-	return score, nil
-}
-
-// Calculates points from length of description
-func pointsFromDescription(receipt Receipt) (int, error) {
-	score := 0
-
-	// Add price * 0.2 if trimmed length of item description % 3 == 0
-	for _, item := range receipt.Items {
-		trimmed := strings.TrimSpace(item.ShortDescription)
-		if len(trimmed)%3 == 0 {
-			price, err := strconv.ParseFloat(item.Price, 64)
-
-			if err != nil {
-				return 0, fmt.Errorf("Error converting price to float: %v\n", err)
-			}
-
-			score += int(math.Ceil(price * 0.2))
-		}
-	}
-
-	return score, nil
-}
-
-// Calculates points based on if day is odd
-func pointsFromDay(receipt Receipt) (int, error) {
-	score := 0
-
-	// Parse date string for day
-	dayString := strings.Split(receipt.PurchaseDate, "-")[2]
-	day, err := strconv.ParseInt(dayString, 10, 8)
-
-	if err != nil {
-		return 0, fmt.Errorf("unable to convert day to int: %v\n", err)
-	}
-
-	// Add 6 points if day in purchase date is odd
-	if day%2 == 1 {
-		score += 6
-	}
-
-	return score, nil
-}
-
-// Calculates the score associated with a given receipt
-func calculateScore(receipt Receipt) (int, error) {
-	score := 0
-
-	// Adds 1 point for each alphanumeric characters of retailer
-	for _, c := range receipt.Retailer {
-		if unicode.IsLetter(c) || unicode.IsDigit(c) {
-			score += 1
-		}
-	}
-
-	// Add points from total
-	points, _ := pointsFromTotal(receipt)
-	score += points
-
-	// Add 5 points for every two items on receipt
-	score += 5 * (len(receipt.Items) / 2)
-
-	// Add points from description length
-	points, _ = pointsFromDescription(receipt)
-	score += points
-
-	// Add points from day value
-	points, _ = pointsFromDay(receipt)
-	score += points
-
-	// Add 10 points if time of purchase is after 2pm and before 4pm
-	time := strings.Split(receipt.PurchaseTime, ":")
-
-	if time[0] == "14" && time[1] != "00" || time[0] == "15" {
-		score += 10
-	}
-
-	return score, nil
-}
-
 func main() {
-	receipt, err := deserialzeJSON("examples/market-receipt.json")
+	receipt, err := deserialzeJSON("mocks/market-receipt.json")
 
 	if err != nil {
-		fmt.Errorf("Error deserializing JSON: %v\n", err)
+		fmt.Errorf("error deserializing JSON: %v", err)
 	}
 
 	score, err := calculateScore(receipt)
 
 	if err != nil {
-		fmt.Errorf("Unable to calculate score: %v\n", err)
+		fmt.Errorf("unable to calculate score: %v", err)
 	}
 
-	fmt.Printf("Total score: %v\n", score)
+	fmt.Printf("Total score: %v", score)
 }
