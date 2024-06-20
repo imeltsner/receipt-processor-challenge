4042450f7a8e182bcbebe351aed7f24b4352d61d
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-06-19 23:23:02 -0700

    updated names and error handling

--- calculate.go
+++ calculate.go
@@ -10,7 +10,7 @@ import (
 
 // Calculates points based on total price
 func pointsFromTotal(receipt Receipt) (int, error) {
-	score := 0
+	points := 0
 
 	// Convert total to float
 	total, err := strconv.ParseFloat(receipt.Total, 64)
@@ -21,20 +21,20 @@ func pointsFromTotal(receipt Receipt) (int, error) {
 
 	// Adds 50 points if total is round dollar amount
 	if int(total*100)%100 == 0 {
-		score += 50
+		points += 50
 	}
 
 	// Add 25 points if total is multiple of 0.25
 	if int(total*100)%25 == 0 {
-		score += 25
+		points += 25
 	}
 
-	return score, nil
+	return points, nil
 }
 
 // Calculates points from length of description
 func pointsFromDescription(receipt Receipt) (int, error) {
-	score := 0
+	points := 0
 
 	// Add price * 0.2 if trimmed length of item description % 3 == 0
 	for _, item := range receipt.Items {
@@ -46,16 +46,16 @@ func pointsFromDescription(receipt Receipt) (int, error) {
 				return 0, fmt.Errorf("error converting price to float: %v", err)
 			}
 
-			score += int(math.Ceil(price * 0.2))
+			points += int(math.Ceil(price * 0.2))
 		}
 	}
 
-	return score, nil
+	return points, nil
 }
 
 // Calculates points based on if day is odd
 func pointsFromDay(receipt Receipt) (int, error) {
-	score := 0
+	points := 0
 
 	// Parse date string for day
 	dayString := strings.Split(receipt.PurchaseDate, "-")[2]
@@ -67,49 +67,52 @@ func pointsFromDay(receipt Receipt) (int, error) {
 
 	// Add 6 points if day in purchase date is odd
 	if day%2 == 1 {
-		score += 6
+		points += 6
 	}
 
-	return score, nil
+	return points, nil
 }
 
 // Calculates the total points associated with a given receipt
 func calculatePoints(receipt Receipt) (int, error) {
-	score := 0
+	points := 0
 
 	// Adds 1 point for each alphanumeric characters of retailer
 	for _, c := range receipt.Retailer {
 		if unicode.IsLetter(c) || unicode.IsDigit(c) {
-			score += 1
+			points += 1
 		}
 	}
 
 	// Add points from total
-	points, err := pointsFromTotal(receipt)
+	p, err := pointsFromTotal(receipt)
 	if err != nil {
 		return 0, err
 	}
-	score += points
+	points += p
 
 	// Add 5 points for every two items on receipt
-	score += 5 * (len(receipt.Items) / 2)
+	points += 5 * (len(receipt.Items) / 2)
 
 	// Add points from description length
-	points, _ = pointsFromDescription(receipt)
-	score += points
-
-	// Add points from day value
-	points, err = pointsFromDay(receipt)
+	p, err = pointsFromDescription(receipt)
 	if err != nil {
 		return 0, err
 	}
-	score += points
+	points += p
+
+	// Add points from day value
+	p, err = pointsFromDay(receipt)
+	if err != nil {
+		return 0, err
+	}
+	points += p
 
 	// Add 10 points if time of purchase is after 2pm and before 4pm
 	time := strings.Split(receipt.PurchaseTime, ":")
 	if time[0] == "14" && time[1] != "00" || time[0] == "15" {
-		score += 10
+		points += 10
 	}
 
-	return score, nil
+	return points, nil
 }
