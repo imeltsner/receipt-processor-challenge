2145ab104326944b7c4360eafe27e1e8bc5d147f
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-06-19 23:53:45 -0700

    changed slice to map for data storage

--- main.go
+++ main.go
@@ -9,46 +9,50 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var receipts []Receipt
+var receipts = map[string]Receipt{}
 
 // Generates an ID for a receipt and returns the ID
 func ProcessReceiptHandler(w http.ResponseWriter, r *http.Request) {
-	// Generate ID and send response
+	// Generate ID
 	id := uuid.New().String()
 
+	// Save receipt with new ID
+	var receipt Receipt
+	err := json.NewDecoder(r.Body).Decode(&receipt)
+
+	if err != nil {
+		http.Error(w, "The receipt is invalid", http.StatusBadRequest)
+		return
+	}
+
+	receipts[id] = receipt
+
+	// Send response
 	response := ID{
 		ID: id,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
-
-	// Save receipt with new ID
-	var receipt Receipt
-	json.NewDecoder(r.Body).Decode(&receipt)
-	receipt.ID = id
-	receipts = append(receipts, receipt)
 }
 
 // Calculates and returns score of receipt given an ID
 func CalculatePointsHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
-	for _, receipt := range receipts {
-		if receipt.ID == params["id"] {
-			score, _ := calculatePoints(receipt)
+	if receipt, ok := receipts[params["id"]]; ok {
+		score, _ := calculatePoints(receipt)
 
-			response := Points{
-				Points: score,
-			}
-
-			json.NewEncoder(w).Encode(response)
-			return
+		response := Points{
+			Points: score,
 		}
 
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		return
 	}
 
-	http.Error(w, "No receipt found for that id", http.StatusBadRequest)
+	http.Error(w, "No receipt found for that id", http.StatusNotFound)
 }
 
 func main() {
